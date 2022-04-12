6d8c0a05b9f40225451c2ab178819d46975bccd8
alion <smirnoviksa@gmail.com>
2022-04-13 03:46:00 +0500

    Create Child model

---
+++ TestTaskApiPrototype/Models/Child.cs
@@ -0,0 +1,12 @@
+﻿namespace TestTaskApiPrototype.Models;
+
+public class Child
+{
+    public string? Name { get; set; }
+
+    public string? Surname { get; set; }
+
+    public string? Patronymic { get; set; }
+
+    public DateTime Dob { get; set; } //TODO: BirthDate?
+}
