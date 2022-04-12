e307333fb74d5cae23aa7fd1e8276db964230947
alion <smirnoviksa@gmail.com>
2022-04-13 03:27:25 +0500

    Create DocumentPassport model

---
+++ TestTaskApiPrototype/Models/DocumentPassport.cs
@@ -0,0 +1,22 @@
+﻿namespace TestTaskApiPrototype.Models;
+
+public class DocumentPassport
+{
+    //TODO: nullable ref type enabled, id it ok?
+    //TODO: add description
+    public Guid Id { get; set; }
+
+    public string? Series { get; set; }
+
+    public string? Number { get; set; }
+
+    public string? Giver { get; set; }
+
+    public DateTime DateIssued { get; set; }
+
+    public string? BirthPlace { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
