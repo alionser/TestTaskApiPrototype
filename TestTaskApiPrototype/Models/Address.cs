5258340da19ef962476399fbe5987909ba079691
alion <smirnoviksa@gmail.com>
2022-04-13 03:36:46 +0500

    Create Address model

---
+++ TestTaskApiPrototype/Models/Address.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTaskApiPrototype.Models;
+
+public class Address
+{
+    public Guid Id { get; set; }
+
+    public string? ZipCode { get; set; }
+
+    [Required]
+    public string? Country { get; set; }
+
+    [Required]
+    public string? Region { get; set; }
+
+    [Required]
+    public string? City { get; set; }
+
+    [Required]
+    public string? Street { get; set; }
+
+    [Required]
+    public string? House { get; set; }
+
+    public string? Block { get; set; }
+
+    public string? Apartment { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
