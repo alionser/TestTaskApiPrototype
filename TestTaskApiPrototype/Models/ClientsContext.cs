3fb2dff53cdf9aade122609dd0305ceba97806ee
alion <smirnoviksa@gmail.com>
2022-04-13 05:18:07 +0500

    Add ClientContext

---
+++ TestTaskApiPrototype/Models/ClientsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestTaskApiPrototype.Models;
+
+public class ClientsContext :DbContext
+{
+    public ClientsContext(DbContextOptions<ClientsContext> options)
+        :base(options)
+    {
+    }
+
+    public DbSet<Client> Clients { get; set; } = null!; //TODO: ClientItems?
+}
--- TestTaskApiPrototype/Program.cs
+++ TestTaskApiPrototype/Program.cs
@@ -1,8 +1,13 @@
+using TestTaskApiPrototype.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ClientsContext>(opt => opt.UseInMemoryDatabase("Clients"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 
