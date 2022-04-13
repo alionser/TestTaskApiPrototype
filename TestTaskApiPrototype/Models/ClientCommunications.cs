04e823a921fd7e411c1587634d6a633ce0987617
alion <smirnoviksa@gmail.com>
2022-04-13 05:03:56 +0500

    Create ClientCommunication model

---
+++ TestTaskApiPrototype/Models/ClientCommunications.cs
@@ -0,0 +1,14 @@
+﻿namespace TestTaskApiPrototype.Models;
+
+public enum ClientCommunicationsType
+{
+    Email = 1,
+    Phone = 2
+}
+
+public class ClientCommunications
+{
+    public ClientCommunicationsType Type { get; set; }
+
+    public string? Value { get; set; }
+}
